1a8890088ed0501b92dea0caf7b256894009c734
chinyong <chhechinyong01@gmail.com>
2020-01-13 14:58:10 +0700

    13-Jan-2020 :Set text to center

--- app.js
+++ app.js
@@ -7,39 +7,48 @@ const app = express();
 const path = require("path");
 const writeText = "add-text-to-image";
 const bodyParser = require("body-parser");
+var colors = require('colors');
+
 var cors = require("cors");
 
 var Jimp = require("jimp");
-var fileName = "./img/index.png";
+var fileName = "./img/index.jpg";
 var loadedImage;
 
-function writeImg(newName) {
-  Jimp.read(fileName)
+ function writeImg(newName) {
+   
+    Jimp.read(fileName)
     .then(function(image) {
       loadedImage = image;
-      return Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+      return (Jimp.loadFont(Jimp.FONT_SANS_32_BLACK));
     })
-    .then(function(font) {
+    .then(  function(font) {
+      var textWidth = Jimp.measureText(font, newName);
+      var textHight = Jimp.measureTextHeight(font, newName);
       loadedImage
         .print(
           font,
-          100,
-          50,
-          {
-            text: newName,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-          },
-          50,
-          60
-        )
-        .write("./img-temp/hello.png" );
-    })
+          loadedImage.bitmap.width/2- textWidth/2,
+          loadedImage.bitmap.height/2- textHight/2, 
+          {   
+            text:newName,
+            //alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,                                                                                                                      
+            //alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+            }, textWidth, textHight)
+        
+           
+        
+        .write("./img-temp/hello.jpg")
+          
+      
+        
+      
     .catch(function(err) {
       console.error(err);
     });
+    
 }
-
+    )}
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -47,15 +56,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.post("/name", (req, res) => {
+  // naming route
+app.post("/name", async(req, res) => {
   const newName = {name: req.body.name};
   console.log(newName.name)
-  writeImg(newName.name);
-
-  setTimeout(() => {
-    res.sendFile(path.join(__dirname + "/img/index.png"));
-  }, 5000);
+  writeImg(newName.name)
+  console.log("hello1")
+  res.sendFile(path.join(__dirname + "/img-temp/hello.jpg"));
+  console.log('hello')
+  //res.sendFile(path.join(__dirname + "/img-temp/hello.png"));
+  
+  //res.send('hello');
+  // setTimeout(() => {
+  //   res.sendFile(path.join(__dirname + "/img-temp/hello.png"));
+  // }, 5000);
+  // res.send("Hello");
 });
+
+
+
+
+
+//Listen on port 3000
 app.listen(3000, () => {
   console.log(chalk.bold.underline.greenBright("http://localhost:3000"));
 });
