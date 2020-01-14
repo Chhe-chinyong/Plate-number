3b9bb2fa3fea140e25df83834f6fa51921afd601
chinyong <chhechinyong01@gmail.com>
2020-01-14 13:25:17 +0700

    config

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var loadedImage;
       loadedImage = image;
       return (Jimp.loadFont(Jimp.FONT_SANS_32_BLACK));
     })
-    .then(  function(font) {
+    .then(function(font) {
       var textWidth = Jimp.measureText(font, newName);
       var textHight = Jimp.measureTextHeight(font, newName);
       loadedImage
@@ -30,25 +30,16 @@ var loadedImage;
           font,
           loadedImage.bitmap.width/2- textWidth/2,
           loadedImage.bitmap.height/2- textHight/2, 
-          {   
-            text:newName,
-            //alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,                                                                                                                      
-            //alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-            }, textWidth, textHight)
-        
+          {text:newName,}, textWidth, textHight)
            
         
-        .write("./img-temp/hello.jpg")
-          
-      
-        
-      
-    .catch(function(err) {
-      console.error(err);
-    });
-    
-}
-    )}
+        .write("./img-temp/hello.jpg")  }  
+        .catch(function(err) {
+      console.error(err);})
+    );
+  }
+
+  
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -60,17 +51,17 @@ app.get("/", (req, res) => {
 app.post("/name", async(req, res) => {
   const newName = {name: req.body.name};
   console.log(newName.name)
-  writeImg(newName.name)
-  console.log("hello1")
-  res.sendFile(path.join(__dirname + "/img-temp/hello.jpg"));
-  console.log('hello')
+  // writeImg(newName.name)
+  // console.log("hello1")
+  // res.sendFile(path.join(__dirname + "/img-temp/hello.jpg"));
+  // console.log('hello')
   //res.sendFile(path.join(__dirname + "/img-temp/hello.png"));
   
-  //res.send('hello');
-  // setTimeout(() => {
-  //   res.sendFile(path.join(__dirname + "/img-temp/hello.png"));
-  // }, 5000);
-  // res.send("Hello");
+  
+  setTimeout(() => {
+    res.sendFile(path.join(__dirname + "/img-temp/hello.png"));
+  }, 5000);
+  res.send("Hello");
 });
 
 
