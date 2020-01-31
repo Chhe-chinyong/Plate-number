57eeab7f0364e37ed070b226eaf456dae5473adb
chinyong <chhechinyong01@gmail.com>
2020-01-31 21:41:51 +0700

    asd

--- routes/post.js
+++ routes/post.js
@@ -23,6 +23,7 @@ router.post("/name", async (req, res) => {
     writeImg(name.toUpperCase());
   }
   setTimeout(() => {
+    console.log(path.join(__dirname, "..", "img-temp/hello.jpg"));
     return res.sendFile(path.join(__dirname, "..", "img-temp/hello.jpg"));
   }, 400);
 });
